3fbd3d0060f14560d873cd1aa037eed3ddf7a5d3
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2021-11-28 17:10:10 -0800

    Add files via upload

---
+++ Chrome Extension/js/contentScript.js
@@ -0,0 +1,13 @@
+
+
+
+ const addTheRecentFbPost = function(){
+ const lnk = document.createElement('iframe');
+ lnk.setAttribute('id', 'lnk');
+ lnk.src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmeta&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" ;
+ document.body.querySelector('div').prepend(lnk)
+ }
+
+
+addTheRecentFbPost()
+
